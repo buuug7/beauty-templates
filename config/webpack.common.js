3388755583bbd4b7bc3dc2ce74ab3fe9c04f4a4e
buuug7 <youpp@126.com>
2019-03-16 00:13:53 +0800

    udpate github pages

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../dist'),
-    publicPath: '/single-page-templates/',
+    publicPath: '/beauty-templates/',
   },
 
   plugins: [
